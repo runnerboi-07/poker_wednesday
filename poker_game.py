3188c1b22a8df3d63cff4d582bdea134c4bc92df
Nathan_Fernandez <nathanf2005@gmail.com>
2025-03-26 17:22:32 +0100

    Commit at break

--- poker_game.py
+++ poker_game.py
@@ -22,15 +22,59 @@ class PokerHand:
                 return False
         return True
 
+    @property
+    def number_of_matches(self):
+        matches = 0
+        for i in range(len(self.cards)):
+            for j in range(len(self.cards)):
+                if i == j:
+                    continue
+                if self.cards[i].rank == self.cards[j].rank:
+                    matches += 1
+        return matches
+
+    @property
+    def is_pair(self):
+        if self.number_of_matches == 2:
+            return True
+        return False
+
+    @property
+    def is_two_pair(self):
+        return self.number_of_matches == 4 # more complicated method
+# ^ shorter; returns True when conditional satisfied & if not, automatically False
+
+    @property
+    def is_three_pair(self):
+        return self.number_of_matches == 6
+
+    @property
+    def is_four_pair(self):
+        return self.number_of_matches == 12
+
+# Full house = if is_three_pair & is_two_pair
+    @property
+    def is_full_house(self):
+        return self.number_of_matches == 8
+
+
 count = 0
-flushes = 0
-while flushes < 1000:
+matches = 0
+while matches < 10000:
     deck = Deck()
     deck.shuffle()
     hand = PokerHand(deck)
-    if hand.is_flush:
-        flushes += 1
+    if hand.is_three_pair:
+        matches += 1
+        # print(hand)
     count += 1
 
-print(f"Probability of a flush is {100*flushes/count}%")
+print(f"Probability of a pair is {100*matches/count}%")
+
+# Probability of each type of pair:
+# single pair ~ 42.405% (10k)
+# two pair ~ 4.719% (10k)
+# three pair ~ 2.091% (10k)
+# four pair ~ 0.026% (100)
+# full_house ~ 0.146% (1k)
 
