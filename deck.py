c9fa1b1737486c3e110f10029638770ffcee08c0
Nathan_Fernandez <nathanf2005@gmail.com>
2025-05-01 02:46:11 +0200

    Added some new docstrings & comments

--- deck.py
+++ deck.py
@@ -2,8 +2,13 @@ import random
 
 class Card:
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-    SUITS = ["♠️", "♥️", "♦️", "♣️"]
+    SUITS = ["♠️", "♥️", "♦️", "♣️"] # RANKS, SUITS are class attributes
     def __init__(self, suit, rank):
+        """
+        Initialising individual card & setting up  error detection
+        :param suit: suit assigned from SUITS
+        :param rank: rank assigned from RANKS
+        """
         if rank not in self.RANKS:
             raise ValueError("Invalid Rank")
         if suit not in self.SUITS:
@@ -12,49 +17,85 @@ class Card:
         self._suit = suit
 
     def __eq__(self, other):
-        return self.rank == other.rank
+        """
+        'Equal to' dunder method to compare self (current instance of the card) with another card according to RANKS
+        :param other: other card
+        :return: boolean comparison
+        """
+        return self.rank == other.rank # .rank is a property defined below
 
     def __gt__(self, other):
         """
-        Compares position of 'self' & 'other' in RANKS list
-        :param other:
-        :return:
+        Compares position of 'self' & 'other' in RANKS list to see which is greater
+        :param other: other card
+        :return: boolean comparison
         """
         return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
 
     def __str__(self): # str for when it will be put in a normal string
+        """
+        Sets how output will be displayed when shown as a string
+        :return: string version displaying rank & suit of the card
+        """
         return f"{self._rank}{self._suit}"
 
     def __repr__(self): # use repr for when it will be put in a list
+        """
+        Sets the representation of self when printed
+        :return: same as string output
+        """
         return self.__str__()
 
     @property
     def suit(self):
+        """
+        Getter method to get value of suit attribute
+        :return: value of self._suit that was initialised
+        """
         return self._suit
 
     @property
     def rank(self):
+        """
+        Getter method to get value of rank attribute
+        :return: value of self._rank that was initialised
+        """
         return self._rank
 
 class Deck:
     def __init__(self):
+        """
+        Deck is initialised; Cards appended to empty list using nested for loops
+        """
         self._deck = []
-        for suit in Card.SUITS:
-            for rank in Card.RANKS:
-                self._deck.append(Card(suit, rank))
+        for suit in Card.SUITS: # first loop to assign suit
+            for rank in Card.RANKS: # second loop to assign rank
+                self._deck.append(Card(suit, rank)) # card created using Card class & appended to list
 
     def __str__(self):
+        """
+        Defines how string output of deck (for this given instance) will be displayed
+        :return: string output
+        """
         return str(self._deck)
 
     def shuffle(self):
+        """
+        Shuffles cards using .shuffle() from the random package; Changes order of the cards, as you would want in a real card game where chance is a factor
+        :return: the original deck, but with the cards in a random order now
+        """
         random.shuffle(self._deck)
 
     def deal(self):
+        """
+        "Deals" a card by extracting the first card in the list; That card is now no longer in the list
+        :return: only the first card in the deck
+        """
         return self._deck.pop(0)
 
-if __name__ == "__main__":
-    deck = Deck()
+if __name__ == "__main__": # this section is only run when this is the main file
+    deck = Deck() # deck created using Deck class; Uses functionalities from Card class within
     print(deck)
-    deck.shuffle()
+    deck.shuffle() # deck shuffled using .shuffle() method defined within the class Deck
     print(deck)
-    print(deck.deal())
+    print(deck.deal()) # first card in shuffled deck is dealt (done using .deal() method defined in class Deck)
--- poker_game.py
+++ poker_game.py
@@ -1,8 +1,11 @@
-from deck import Deck, Card
+from deck import Deck, Card # Creating cards & deck is essential to starting the game - can import the classes we already set up in deck.py
 
 class PokerHand:
     def __init__(self, deck):
-
+        """
+        Initialise deck with 5 cards for Poker
+        :param deck: Deck that was created & shuffled using Deck class will be used to deal
+        """
         cards = []
         for i in range(5):
             cards.append(deck.deal())
@@ -10,72 +13,114 @@ class PokerHand:
 
     @property
     def cards(self):
+        """
+        Getter method for cards list
+        :return: list from self._cards
+        """
         return self._cards
 
     def __str__(self):
+        """
+        Defining string display of cards list (with 5 cards)
+        :return: string output
+        """
         return str(self._cards)
 
-    @property
+# Checking for flush,
+    @property # turns is_flush into a read-only property
     def is_flush(self):
-        for card in self.cards[1:]:
+        """
+        Checking whether there is a flush in the 5 current five card hand
+        :return: Boolean True or False
+        """
+        for card in self.cards[1:]: # loop from second card onwards
             if self.cards[0].suit != card.suit: # comparing 1st card suit to every other card suit
                 return False
         return True
 
     @property
     def number_of_matches(self):
+        """
+        Counting number of matches in a hand based on how many cards have the same rank
+        :return: number of matches
+        """
         matches = 0
         for i in range(len(self.cards)):
-            for j in range(len(self.cards)):
+            for j in range(len(self.cards)): # two loops mean each match is double counted (e.g. one pair is two matches, three of a kind is six matches & four of a kind is twelve matches)
                 if i == j:
-                    continue
+                    continue # skips comparing the card with itself
                 if self.cards[i].rank == self.cards[j].rank:
-                    matches += 1
+                    matches += 1 # add 1 to match when two different cards in the hand have the same rank
         return matches
 
     @property
     def is_pair(self):
-        if self.number_of_matches == 2:
+        """
+        Checks whether there is one pair in the hand using number_of_matches property defined just before
+        :return: Boolean True or False
+        """
+        if self.number_of_matches == 2: # one pair has two matches as per our number_of_matches property which uses double counting
             return True
         return False
 
     @property
     def is_two_pair(self):
-        return self.number_of_matches == 4 # more complicated method
-# ^ shorter; returns True when conditional satisfied & if not, automatically False
+        """
+        Checks whether there are two separate pairs using number_of_matches
+        :return: Boolean True or False
+        """
+        return self.number_of_matches == 4 # more complicated statement structure
+# statement structure above is shorter than if, else; Returns True when conditional is satisfied & if not, automatically False
 
     @property
     def is_three_pair(self):
+        """
+        Checks whether there is a group of three identical rank cards using number_of_matches
+        :return: Boolean True or False
+        """
         return self.number_of_matches == 6
 
     @property
     def is_four_pair(self):
+        """
+        Checks whether there are four cards with an identical rank using number_of_matches
+        :return: Boolean True or False
+        """
         return self.number_of_matches == 12
 
-# Full house = if is_three_pair & is_two_pair
+# Full house = if is_three_pair & is_pair
     @property
     def is_full_house(self):
-        return self.number_of_matches == 8
+        """
+        Checks for a full house --> when there is a three of a kind + a pair
+        :return: True or False
+        """
+        return self.number_of_matches == 8 # 6 matches from three of a kind + 2 matches from a pair
 
     @property
     def is_straight(self):
-        self.cards.sort()
+        """
+        Checks for a straight when five cards of any suit are in sequential order of rank
+        :return:
+        """
+        self.cards.sort() # can sort by rank since __eq__ & __gt__ defined for Card class
         distance = Card.RANKS.index(self.cards[4].rank) - \
-                   Card.RANKS.index(self.cards[0].rank)
-        return self.number_of_matches == 0 and distance == 4
+                   Card.RANKS.index(self.cards[0].rank) # if it is a 'straight' then when sorted the difference between the largest rank card and smallest rank card will be 4 (since there will only be five cards, all in order)
+        return self.number_of_matches == 0 and distance == 4 # if 'straight', all cards will be different + (as explained above) difference between largest & smallest card rank in sorted hand will be 4
 
-count = 0
-matches = 0
-while matches < 1000:
-    deck = Deck()
-    deck.shuffle()
-    hand = PokerHand(deck)
-    if hand.is_straight:
-        matches += 1
-        print(hand)
-    count += 1
+if __name__ == "__main__":
+    count = 0 # number of times loop is run
+    matches = 0 # number of times the characteristic we are seeking has occurred in these loops
+    while matches < 1000: # keeps checking while total number of straights found is less than 1000
+        deck = Deck()
+        deck.shuffle()
+        hand = PokerHand(deck)
+        if hand.is_straight:
+            matches += 1 # matches increased by one when hand is a straight
+            print(hand)
+        count += 1 # count increased by one regardless of whether it is a straight or not
 
-print(f"Probability of a straight flush is {100*matches/count}%")
+    print(f"Probability of a straight flush is {100*matches/count}%")
 
 # Probability of each type of pair:
 # single pair ~ 42.405% (10k)
